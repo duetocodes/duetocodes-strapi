0bac779ce08e39139d865d59e40945e605fc9021
duetocodes <duetocodes@outlook.com>
2025-11-15 21:39:42 +0800

    refactor(strapi): convert tech stack tags from text to relational structure

---
+++ src/api/tech-stack-tag/routes/tech-stack-tag.ts
@@ -0,0 +1,7 @@
+/**
+ * tech-stack-tag router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::tech-stack-tag.tech-stack-tag');
---
+++ src/api/tech-stack-tag/services/tech-stack-tag.ts
@@ -0,0 +1,7 @@
+/**
+ * tech-stack-tag service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::tech-stack-tag.tech-stack-tag');
